0ac619c9870ebb6f679687a4327ffdb91d2b8af3
Sarah Pohorecky <spohorec@mit.edu>
2017-07-13 02:01:07 -0400

    Updated with new pinouts.

--- acid.h
+++ acid.h
@@ -4,33 +4,47 @@
 	
 
 	@date 2017-07-08 creation
+	@date 2017-07-13 updates with new pinouts
 
 
 **/
+
 #include <Arduino.h>
 
 #include "commander.h"
 #include "motorinterface.h"
-// #include "throttle.h"
 
 #define TRIPPING true
 
-//E PWM Output pins
-	//E On Arduino Uno: can only use 3, 5, 6, 9, 10, and 11
-	//E On Arduino Mega: only 2-13 and 44-46
-#define P_FIELD_PWM 11
-#define P_MOTOR_PWM 3 
-#define P_REGEN_PWM 5
+#define P_AUTONOMY_SWITCH 1 //E Physical mode switch input pin
 
-//E Analog Input Pins (Throttles)
-#define P_FIELD_THROTTLE A2
-#define P_MOTOR_THROTTLE A3
+#define P_REGEN_PWM 10 //E Regen output pin
+#define P_BRAKE_1 7 //E Physical brake switch 1 input pin
+#define P_BRAKE_2 8 //E Physical brake switch 2 input pin
 
-//E Throttle Ranges
-#define MOTOR_THROTTLE_MIN 0.85
+#define P_REVERSE_SWITCH 0 //E Physical reverse switch input pin
+#define P_SET_REVERSE 12 //E Kelly reverse switch command output pin
+	
+#define P_MOTOR_PWM 9 //E Kelly motor command output pin
+#define P_MOTOR_THROTTLE A0 //E Motor throttle input pin
+#define MOTOR_THROTTLE_MIN 0.85 //E Throttle Ranges
 #define MOTOR_THROTTLE_MAX 4.35
-#define FIELD_THROTTLE_MIN 0.85
+
+#define P_ENCODER 2 //E Encoder input pin
+
+#define P_FIELD_PWM 11 //E Field driver command output pin
+#define P_FIELD_THROTTLE A1 //E Field Throttle input pin
+#define FIELD_THROTTLE_MIN 0.85 //E Throttle Ranges
 #define FIELD_THROTTLE_MAX 4.35
+
+#define P_THERMISTOR A3 //E Thermistor input pin
+#define P_TEMP_INDICATOR 4 //E Overheating indicator output pin
+
+#define P_SET_SERVO 13 //E MegaMoto enable command output pin
+#define P_SERVO_PWM_A 6 //E Servo PWM A pin
+#define P_SERVO_PWM_B 5 //E Servo PWM B pin
+#define P_SERVO_POT A2 //E Servo pot input pin
+	
 
 class Acid {
 public:
@@ -41,6 +55,7 @@ private:
 	Throttle _motor_throttle, _field_throttle;
 	MotorInterface _kelly;
 	FieldInterface _field;
-	PhysCommander _commander;
+	PhysCommander _pcommander;
+	Commander *_commander;
 
 };
